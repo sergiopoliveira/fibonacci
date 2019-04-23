d80fd5d9ca342bc3c73e59e833450a5ffdbee729
sergiopoliveira <sergiopoliveira@outlook.com>
2019-04-23 20:25:37 +0100

    rendering logic in the React app

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -15,7 +15,7 @@ class Fib extends Component {
 
     async fetchValues() {
         const values = await axios.get('/api/values(current');
-        this.setState({ values: values.data});
+        this.setState({values: values.data});
     }
 
     async fetchIndexes() {
@@ -23,5 +23,51 @@ class Fib extends Component {
         this.setState({
             seenIndexes: seenIndexes.data
         });
+    }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+
+        await axios.post('/api/values', {
+            index: this.state.index
+        });
+        this.setState({index: ''});
+    };
+
+    renderSeenIndexes() {
+        return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    }
+
+    renderValues() {
+        const entries = [];
+
+        for(let key in this.state.values) {
+            entries.push(
+                <div key={key}>
+                    For index {key}  I calculated {this.state.values[key]}
+                </div>
+            );
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Enter your index:</label>
+                    <input
+                        value={this.state.index}
+                        onChange={event => this.setState({index: event.target.value})}
+                   />
+                    <buton>Submit</buton>
+                </form>
+
+                <h3>Indexes I have seen:</h3>
+                {this.renderSeenIndex()}
+
+                <h3>Calculated Values:</h3>
+                {this.renderValues()}
+            </div>
+        )
     }
 }
